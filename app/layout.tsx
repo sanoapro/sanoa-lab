ef97f94b0a033e4a6b9979117e08710a186a4650
Sanoa <martin.maldonado32@gmail.com>
2025-09-06 18:46:37 +0000

    nuevo commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
+// app/layout.tsx
 import type { Metadata } from "next";
 import "./globals.css";
 import { Poppins, Lato } from "next/font/google";
+
+import { EmojiProvider } from "@/components/EmojiTheme";
+import { emojiTheme } from "@/config/emojiTheme";
 
 /* Cargamos fuentes y exponemos variables CSS:
    --font-poppins y --font-lato (las usamos en @theme) */
@@ -27,9 +31,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="es">
+    <html
+      lang="es"
+      // Este atributo es opcional, pero útil si quisieras forzar modo desde HTML:
+      data-emoji-mode={emojiTheme.global.mode}
+    >
       <body className={`${poppins.variable} ${lato.variable} font-body text-brand-text bg-brand-background`}>
-        {children}
+        <EmojiProvider
+          mode={emojiTheme.global.mode}
+          color={emojiTheme.global.color}
+          accentColor={emojiTheme.global.accentColor}
+          perEmoji={emojiTheme.perEmoji}
+        >
+          {children}
+        </EmojiProvider>
       </body>
     </html>
   );
