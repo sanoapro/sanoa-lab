a840259507569d5455c192f9e973f0bfe4375d23
Sanoa <martin.maldonado32@gmail.com>
2025-09-30 17:13:29 -0600

    Improve specialties access messaging

--- app/(app)/areas/page.tsx
+++ app/(app)/areas/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from "next/navigation";
+import { permanentRedirect } from "next/navigation";
 
 export default function AreasPage() {
-  redirect("/especialidades");
+  permanentRedirect("/especialidades");
 }
